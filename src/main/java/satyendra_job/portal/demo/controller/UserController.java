d9b44e74db3187b8cfb95f152c7b7e751bb94efa
SatendraPratap-Singh <singhsatyendra.singh00@gmail.com>
2026-04-02 14:39:40 +0530

    Add files via upload

---
+++ src/main/java/satyendra_job/portal/demo/controller/UserController.java
@@ -0,0 +1,32 @@
+package satyendra_job.portal.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+import satyendra_job.portal.demo.model.JobPlan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/jobplans")
+public class UserController {
+
+    // 🟢 Temporary storage
+    private List<JobPlan> jobList = new ArrayList<>();
+
+    // 🟢 SAVE DATA
+    @PostMapping
+    public JobPlan createJobPlan(@RequestBody JobPlan jobPlan) {
+
+        jobList.add(jobPlan); // save in memory
+
+        return jobPlan;
+    }
+
+    // 🟢 GET ALL DATA
+    @GetMapping
+    public List<JobPlan> getAllJobPlans() {
+        return jobList;
+    }
+
+}
